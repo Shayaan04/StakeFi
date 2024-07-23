8967aa8503033fa380de752388c62e11eaaa9f1c
Shayaan Mohammed <shayaanm2005@gmail.com>
2024-07-23 02:46:06 -0500

    changed colors and integrated the new logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import bank from '../bank.png'
+import bank from '../Slogo.png'
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className = 'navbar navbar-dark fixed-top shadow p-0' style = {{backgroundColor: 'black', height:'50px'}} >
-                <a className = 'navbar-brand col-sm-3 col-md-2 mr-0'
-                style = {{color: 'white'}}>
-                <img src = {bank} width = '50' height = '30' className = 'd-inline-block align-top' alt = 'bank'/>
-                &nbsp; DAPP Yield Staking (Decentralized Banking) 
-                </a>
-                <ul className = 'navbar-nav px-3'>
-                    <li className = 'text-nowrap d-none nav-item d-sm-none d-sm-block'>
-                        <small id = '' style = {{color: 'white'}}> ACCOUNT NUMBER: {this.props.account}
-                        </small>
-                    </li>
+            <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor: 'white', height: '50px'}}>
+  <button className='navbar-brand col-sm-3 col-md-2 mr-0'
+          style={{color: 'black', background: 'none', border: 'none', display: 'flex', alignItems: 'center', marginLeft: '20px'}}>
+    <img src={bank} width='50' height='30' className='d-inline-block align-top' alt='Slogo'/>
+    <span style={{marginLeft: '5px'}}>STAKEFI</span>
+  </button>
+  <ul className='navbar-nav px-3'>
+    <li className='text-nowrap d-none nav-item d-sm-none d-sm-block'>
+      <small id='' style={{color: 'black'}}> ACCOUNT NUMBER: {this.props.account}
+      </small>
+    </li>
+  </ul>
+</nav>
 
-
-
-                </ul>
-
-
-            </nav>
         )
     }
 }
