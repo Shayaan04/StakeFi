a12952f090ecd07e90e122a0429a0f318ed9a997
Shayaan Mohammed <shayaanm2005@gmail.com>
2024-07-23 15:34:59 -0500

    DONE

---
+++ build/precache-manifest.f1db3b768e712548c652176308146a49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c66cca0fcc0c6e9b2bd7",
+    "url": "/static/css/main.e7c10e4c.chunk.css"
+  },
+  {
+    "revision": "c66cca0fcc0c6e9b2bd7",
+    "url": "/static/js/main.c66cca0f.chunk.js"
+  },
+  {
+    "revision": "8b3665a7b8ce8370fe7a",
+    "url": "/static/css/1.7d3c752e.chunk.css"
+  },
+  {
+    "revision": "8b3665a7b8ce8370fe7a",
+    "url": "/static/js/1.8b3665a7.chunk.js"
+  },
+  {
+    "revision": "97617defaa5f2061d989",
+    "url": "/static/js/2.97617def.chunk.js"
+  },
+  {
+    "revision": "2a91f14e6d65c23ba616",
+    "url": "/static/js/runtime~main.2a91f14e.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "d6e911a0391b883acb41432a168a4c6b",
+    "url": "/index.html"
+  }
+];
