7272f8fb2316f5a6ecb1a3a699e3be07298e5779
Shayaan Mohammed <shayaanm2005@gmail.com>
2024-07-23 22:07:26 -0500

    Updates

---
+++ precache-manifest.d8cdc986b23203fb5073e651cb486f97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c9685a947ae7a9a10a9",
+    "url": "/StakeFi/static/css/main.e7c10e4c.chunk.css"
+  },
+  {
+    "revision": "0c9685a947ae7a9a10a9",
+    "url": "/StakeFi/static/js/main.0c9685a9.chunk.js"
+  },
+  {
+    "revision": "2e6fb3e18157e407111f",
+    "url": "/StakeFi/static/css/1.7d3c752e.chunk.css"
+  },
+  {
+    "revision": "2e6fb3e18157e407111f",
+    "url": "/StakeFi/static/js/1.2e6fb3e1.chunk.js"
+  },
+  {
+    "revision": "97617defaa5f2061d989",
+    "url": "/StakeFi/static/js/2.97617def.chunk.js"
+  },
+  {
+    "revision": "cdefc169392b345b71a4",
+    "url": "/StakeFi/static/js/runtime~main.cdefc169.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/StakeFi/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "8757f79a397141d2f3f7b1ef64dceec7",
+    "url": "/StakeFi/index.html"
+  }
+];
